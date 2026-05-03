4341537afbda4929538f2de65b58b733c2dd5834
Avishek-Pratyay <avishekp.aiub@gmail.com>
2026-05-03 22:27:42 +0600

    api route complete

--- app/api/test-db/route.js
+++ app/api/test-db/route.js
@@ -1,6 +1,6 @@
-import { connectDB } from "@/lib/db";
+/*import { connectDB } from "@/lib/db";
 
 export async function GET() {
   await connectDB();
   return Response.json({ message: "MongoDB Connected ✅" });
-}
+}*/
